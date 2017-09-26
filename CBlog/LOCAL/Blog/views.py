18c0fba7ced3b070945d2edb988aa245c717980f
FAESGAR-PC\FAESGAR <fwiktora@crosstales.com>
2017-09-26 13:01:50 +0200

    - Basic blog implemented

--- CBlog/LOCAL/Blog/views.py
+++ CBlog/LOCAL/Blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
+# Dot = current dir. Imports the Post model.
+from .models import Post
 
-# Create your views here.
+""" More Info on QuerySets:
+ https://tutorial.djangogirls.org/en/django_orm/
+ https://docs.djangoproject.com/en/1.9/ref/models/querysets/ """
+
+# Put together Blog/post_list.html template
+def post_list(request):
+     # QuerySet
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
